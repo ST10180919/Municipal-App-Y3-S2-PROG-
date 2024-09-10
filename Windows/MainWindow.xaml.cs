bac886458f4d075c65e4ca8f7e0ac67643dd514a
ST10180919 <st10180919@vcconnect.edu.za>
2024-09-10 14:16:18 +0200

    Completed header styling and functionality

--- Windows/MainWindow.xaml.cs
+++ Windows/MainWindow.xaml.cs
@@ -24,5 +24,54 @@ namespace Municipal_App
         {
             InitializeComponent();
         }
+
+        //-----------------------------------------------------------------------------
+        /// <summary>
+        /// Allows the user to drag the Window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Header_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        //-----------------------------------------------------------------------------
+        /// <summary>
+        /// Minimizes the application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        //-----------------------------------------------------------------------------
+        /// <summary>
+        /// Changes the window state Maximise to minimise and vice versa
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonChangeWindowState_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow.WindowState != WindowState.Maximized)
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+            }
+        }
+
+        //-----------------------------------------------------------------------------
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
